cff497a0e79dcc4502515beabc09e5a07bbf9392
Sayed Elyas Sadat <elyassadat07@gmail.com>
2024-08-16 21:23:58 -0700

    Wired Apex

---
+++ force-app/main/default/lwc/wiredApex1/wiredApex1.js
@@ -0,0 +1,19 @@
+import getTopAccounts from '@salesforce/apex/AccountCtrl.getTopAccounts';
+import { LightningElement, wire } from 'lwc';
+
+const COLUMNS=[
+    {label:"Account Name", fieldName:"Name", type:"text"},
+    {label:"Type", fieldName:"Type", type:"text"},
+    {label:"Industry", fieldName:"Industry", type:"text"},
+    {label:"Annual Revenue", fieldName:"AnnualRevenue", type:"currency"}
+    
+]
+
+export default class WiredApex1 extends LightningElement {
+    // call the apex method 1st
+
+    columns= COLUMNS;
+
+    @wire(getTopAccounts)
+    accounts;
+}
